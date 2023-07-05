b0774b8c7d16380c4abab160bccc7e49a127c395
Dhananjay-12 <pantpriyanshu31@gmail.com>
2023-07-05 22:07:07 +0530

    react quiz files

---
+++ React-quiz/react-quiz/src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./Header";
+import MainSection from "./MainSection";
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <MainSection>
+        <p>1/15</p>
+        <p>question </p>
+      </MainSection>
+    </div>
+  );
+}
+
+export default App;
---
+++ React-quiz/react-quiz/src/MainSection.jsx
@@ -0,0 +1,5 @@
+function MainSection({ children }) {
+  return <main>{children}</main>;
+}
+
+export default MainSection;
