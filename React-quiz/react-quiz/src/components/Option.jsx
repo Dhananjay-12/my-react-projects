a211e557e02658c236b5838079b7306a182793e8
Dhananjay-12 <pantpriyanshu31@gmail.com>
2023-07-06 14:13:44 +0530

    functionality to pass option

--- React-quiz/react-quiz/src/components/Option.jsx
+++ React-quiz/react-quiz/src/components/Option.jsx
@@ -1,10 +1,22 @@
 import PropTypes from "prop-types";
 
-export default function Option({ question }) {
+export default function Option({ question, answer, dispatch }) {
+  const hasAnswered = answer !== null;
   return (
     <div className="options">
       {question?.options.map((option, key) => (
-        <button key={key} className="btn btn-option">
+        <button
+          key={option}
+          className={`btn btn-option ${key === answer ? "answer" : ""} ${
+            hasAnswered
+              ? key === question.correctOption
+                ? "correct"
+                : "wrong"
+              : ""
+          }`}
+          disabled={hasAnswered}
+          onClick={() => dispatch({ type: "newAnswer", payload: key })}
+        >
           {option}
         </button>
       ))}
@@ -14,9 +26,9 @@ export default function Option({ question }) {
 
 Option.propTypes = {
   question: PropTypes.shape({
-    question: PropTypes.string.isRequired,
-    options: PropTypes.arrayOf(PropTypes.string).isRequired,
-    correctOption: PropTypes.number.isRequired,
-    points: PropTypes.number.isRequired,
+    question: PropTypes.string,
+    options: PropTypes.array,
+    answer: PropTypes.number,
+    dispatch: PropTypes.func,
   }).isRequired,
 };
