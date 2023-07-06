4b6d3c6ea9594c7c9d1465509d4959e285ff2afa
Dhananjay-12 <pantpriyanshu31@gmail.com>
2023-07-06 14:14:17 +0530

    prop drilling level 1

--- React-quiz/react-quiz/src/components/Question.jsx
+++ React-quiz/react-quiz/src/components/Question.jsx
@@ -1,12 +1,12 @@
 import PropTypes from "prop-types";
 import Option from "./Option";
 
-export default function Question({ question }) {
+export default function Question({ question, answer, dispatch }) {
   console.log(question);
   return (
     <div>
       <h3>{question.question}</h3>
-      <Option question={question} />;
+      <Option question={question} answer={answer} dispatch={dispatch} />;
     </div>
   );
 }
