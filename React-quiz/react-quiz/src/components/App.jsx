3382e2b65144c285732ee7b4c7904da992e4d233
Dhananjay-12 <pantpriyanshu31@gmail.com>
2023-07-06 14:13:15 +0530

    added points logic after submitting the answer

--- React-quiz/react-quiz/src/components/App.jsx
+++ React-quiz/react-quiz/src/components/App.jsx
@@ -12,6 +12,8 @@ const initialState = {
   // 'loading' , 'error' , 'ready' , 'active' , 'finished'S
   status: "loading",
   index: 0,
+  answer: null,
+  points: 0,
 };
 function reducer(state, action) {
   switch (action.type) {
@@ -33,6 +35,18 @@ function reducer(state, action) {
         ...state,
         status: "active",
       };
+    case "newAnswer": {
+      const question = state.questions.at(state.index);
+
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
+    }
 
     default:
       throw new Error("Action Unknown");
@@ -40,7 +54,7 @@ function reducer(state, action) {
 }
 
 function App() {
-  const [{ questions, status, index }, dispatch] = useReducer(
+  const [{ questions, status, index, answer }, dispatch] = useReducer(
     reducer,
     initialState
   );
@@ -67,7 +81,13 @@ function App() {
         {status === "ready" && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status === "active" && <Question question={questions[index]} />}
+        {status === "active" && (
+          <Question
+            question={questions[index]}
+            answer={answer}
+            dispatch={dispatch}
+          />
+        )}
       </MainSection>
     </div>
   );
