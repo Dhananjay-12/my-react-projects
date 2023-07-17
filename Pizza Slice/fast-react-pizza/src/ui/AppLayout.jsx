65a30dbd14688862f3aeb126e98a6b4f6777dcc4
Dhananjay-12 <pantpriyanshu31@gmail.com>
2023-07-17 19:07:50 +0530

    display loader component

--- Pizza Slice/fast-react-pizza/src/ui/AppLayout.jsx
+++ Pizza Slice/fast-react-pizza/src/ui/AppLayout.jsx
@@ -1,10 +1,17 @@
 import Header from "./Header";
 import CartOverView from "../features/cart/CartOverview";
-import { Outlet } from "react-router-dom";
+import Loader from "../ui/Loader";
+
+import { Outlet, useNavigation } from "react-router-dom";
 export default function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
   return (
     <div>
+      {isLoading && <Loader />}
+
       <Header />
+
       <main>
         <h1>Content</h1>
         <Outlet />
