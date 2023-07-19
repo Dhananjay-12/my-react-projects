2ce51f92cdd2b9ac2641273a7f0c97e8ea4c0293
Dhananjay-12 <pantpriyanshu31@gmail.com>
2023-07-19 19:44:48 +0530

    styled loader component

--- Pizza Slice/fast-react-pizza/src/features/cart/Cart.jsx
+++ Pizza Slice/fast-react-pizza/src/features/cart/Cart.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import LinkButton from "../../ui/LinkButton";
+import Button from "../../ui/Button";
 
 const fakeCart = [
   {
@@ -33,7 +34,8 @@ function Cart() {
       <LinkButton to="/menu"> &larr; Back to menu</LinkButton>
       <h2>Your cart, %NAME%</h2>
       <div>
-        <Link to="/order/new">Order pizzas</Link>
+        {/* <Link to="/order/new">Order pizzas</Link> */}
+        <Button to="/order/new">Order Pizzas</Button>
         <button>Clear cart</button>
       </div>
     </div>
--- Pizza Slice/fast-react-pizza/src/ui/Button.jsx
+++ Pizza Slice/fast-react-pizza/src/ui/Button.jsx
@@ -1,10 +1,17 @@
-export default function Button({ children, disabled }) {
+import { Link } from "react-router-dom";
+
+export default function Button({ children, disabled, to }) {
+  const className =
+    "inline-block py- trackin rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-all duration-300 hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-4";
+  if (to)
+    return (
+      <Link className={className} to={to}>
+        {children}
+      </Link>
+    );
+
   return (
-    <button
-      className="py- trackin rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-all duration-300 hover:bg-yellow-300 
-            focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-4"
-      disabled={disabled}
-    >
+    <button className={className} disabled={disabled}>
       {children}
     </button>
   );
