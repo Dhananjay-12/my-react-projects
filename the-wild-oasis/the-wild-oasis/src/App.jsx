81f69e4727f5f3e908a059371e1a159f745cfefe
Dhananjay-12 <pantpriyanshu31@gmail.com>
2023-07-22 18:04:36 +0530

    setup project, routes anapp layout

--- the-wild-oasis/the-wild-oasis/src/App.jsx
+++ the-wild-oasis/the-wild-oasis/src/App.jsx
@@ -1,5 +1,38 @@
-function App() {
-  return <h1>hello</h1>;
-}
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+//pages
+import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+import Settings from "./pages/Settings";
+import Users from "./pages/Users";
+import Account from "./pages/Account";
 
-export default App;
+//UI
+import AppLayout from "./ui/AppLayout";
+
+import GlobalStyles from "./styles/GlobalStyles";
+
+export default function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="users" element={<Users />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="account" element={<Account />} />
+          </Route>
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
