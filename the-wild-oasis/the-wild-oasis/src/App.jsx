7e419fc23e2f6783f67d98561677089b84fd6bdf
Dhananjay-12 <pantpriyanshu31@gmail.com>
2023-07-23 14:00:08 +0530

    setup ract query and its devtool

--- the-wild-oasis/the-wild-oasis/src/App.jsx
+++ the-wild-oasis/the-wild-oasis/src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 //pages
 import Dashboard from "./pages/Dashboard";
 import Bookings from "./pages/Bookings";
@@ -15,8 +17,16 @@ import AppLayout from "./ui/AppLayout";
 import GlobalStyles from "./styles/GlobalStyles";
 
 export default function App() {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 60 * 1000,
+      },
+    },
+  });
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
@@ -33,6 +43,6 @@ export default function App() {
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
-    </>
+    </QueryClientProvider>
   );
 }
