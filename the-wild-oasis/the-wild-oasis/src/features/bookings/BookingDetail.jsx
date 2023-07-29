6e815ee63de801204e7b87a4a7512f0919d9c1c7
Dhananjay-12 <pantpriyanshu31@gmail.com>
2023-07-29 22:35:59 +0530

    booking detail page added

--- the-wild-oasis/the-wild-oasis/src/features/bookings/BookingDetail.jsx
+++ the-wild-oasis/the-wild-oasis/src/features/bookings/BookingDetail.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 
+import Spinner from "../../ui/Spinner";
 import BookingDataBox from "./BookingDataBox";
 import Row from "../../ui/Row";
 import Heading from "../../ui/Heading";
@@ -9,6 +10,7 @@ import Button from "../../ui/Button";
 import ButtonText from "../../ui/ButtonText";
 
 import { useMoveBack } from "../../hooks/useMoveBack";
+import { useBooking } from "./useBooking";
 
 const HeadingGroup = styled.div`
   display: flex;
@@ -17,11 +19,11 @@ const HeadingGroup = styled.div`
 `;
 
 function BookingDetail() {
-  const booking = {};
-  const status = "checked-in";
-
+  const { isLoading, booking, error } = useBooking();
   const moveBack = useMoveBack();
 
+  if (isLoading) return <Spinner />;
+  const { status, id: bookingId } = booking;
   const statusToTagName = {
     unconfirmed: "blue",
     "checked-in": "green",
@@ -32,7 +34,7 @@ function BookingDetail() {
     <>
       <Row type="horizontal">
         <HeadingGroup>
-          <Heading as="h1">Booking #X</Heading>
+          <Heading as="h1">Booking #{bookingId}</Heading>
           <Tag type={statusToTagName[status]}>{status.replace("-", " ")}</Tag>
         </HeadingGroup>
         <ButtonText onClick={moveBack}>&larr; Back</ButtonText>
